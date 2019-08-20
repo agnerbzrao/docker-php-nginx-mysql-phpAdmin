589910178e4f60e5488f2080835a5ff3801da245
Agner Bezerra <agner.bezerra@pravaler.com.br>
2019-08-20 16:21:12 -0300

    first commit

---
+++ www/index.php
@@ -0,0 +1,17 @@
+<?php
+	try {
+        $conn = new PDO( 'mysql:host=db;dbname=teste', 'teste', '123456');
+		$sql = 'SELECT * FROM usuarios';
+ 
+		foreach( $conn->query( $sql ) as $row ) {
+			echo '<li>' . $row['nome'] . '</li>';
+		}
+ 
+		$conn = null;
+ 
+	} catch (PDOException $e) {
+		echo "Erro!: " . $e->getMessage() . "<br/>";
+		die();
+	}    
+?>
+ 
